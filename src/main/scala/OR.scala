def6b597daeb48d0b0c0d01d7b69d2b2b16c202e
saika <sxp42750@ucmo.edu>
2024-08-15 10:59:25 -0400

    Range check with OR

---
+++ src/main/scala/OR.scala
@@ -0,0 +1,9 @@
+object OR {
+  def main(args:Array[String]):Unit={
+    val num = -9;
+    if(num < -10 || num >10)
+      print("the number is either less than -10 or greater than 10")
+      else
+        print("the num is greater than -10 or less than 10 ")
+  }
+}
