53e8570c65d331d630358853f65c19773133aee2
saika <sxp42750@ucmo.edu>
2024-08-15 16:37:17 -0400

    Check for Divisibility for 4 OR 6

---
+++ src/main/scala/Divisiblity4OR6.scala
@@ -0,0 +1,10 @@
+object Divisiblity4OR6 {
+  def main(args:Array[String]):Unit={
+    val num = 24;
+    if(num%4==0 || num%6==0)
+      print("number is divisible by either 4 or 6")
+    else
+      print("Number is neither divisible by 4 or 6")
+
+  }
+}
