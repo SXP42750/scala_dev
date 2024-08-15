a6f7418d0a54d2b748438cb9241b0c03e8b3c86e
saika <sxp42750@ucmo.edu>
2024-08-15 16:32:45 -0400

    check for Non-Negative AND Not Divisible by 7

---
+++ src/main/scala/NonNegativeNOTDivsibleBy7.scala
@@ -0,0 +1,10 @@
+object NonNegativeNOTDivsibleBy7 {
+  def main(args:Array[String]):Unit={
+    val num = 14;
+    if(num >0 && num%7!=0)
+      print("The Given Number is non-negative and not divisible by 7")
+    else
+      print("Number is positive and divisible by 7")
+
+  }
+}
