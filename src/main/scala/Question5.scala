27c8c387d29cdba626ac2d71eb644b78cc9014d1
saika <sxp42750@ucmo.edu>
2024-08-15 13:58:49 -0400

    Eligibility for Voting OR Driving

---
+++ src/main/scala/Question5.scala
@@ -0,0 +1,10 @@
+object Question5 {
+  def main(args:Array[String]):Unit={
+    val age = 25;
+    if(age >= 18 || age >= 16)
+      print("the person is eligible for voting or driving")
+    else
+      print("The person is not eligible for either voting or driving")
+
+  }
+}
