6d7b361a8f6ec08745d7a1495ad8b320f06047d1
saika <sxp42750@ucmo.edu>
2024-08-15 13:51:44 -0400

    Even AND Positive Number

---
+++ src/main/scala/EvenPositive.scala
@@ -0,0 +1,10 @@
+object EvenPositive {
+  def main(args:Array[String]):Unit={
+    val num = 15;
+    if(num%2==0 && num >0)
+      print("Number is even and positive")
+    else
+      print("Number is odd")
+
+  }
+}
