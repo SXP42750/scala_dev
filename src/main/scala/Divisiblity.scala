b16f7e62bf40c31bf7ad44f4a96971843bd86fcc
saika <sxp42750@ucmo.edu>
2024-08-15 15:46:20 -0400

    Divisibility by 5 and 7

---
+++ src/main/scala/Divisiblity.scala
@@ -0,0 +1,10 @@
+object Divisiblity {
+  def main(args:Array[String]):Unit={
+    val num = 35;
+    if(num%5 ==0 && num %7 ==0)
+      print("The number is divisible by both 5 and 7");
+    else
+      print("The number is neither divisible by 5 and 7")
+
+  }
+}
