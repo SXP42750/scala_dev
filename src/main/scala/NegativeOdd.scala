aa3792ee4af160155a929fce7b4b04a8627b754d
saika <sxp42750@ucmo.edu>
2024-08-15 14:00:37 -0400

    Check for Negative AND Odd Number

---
+++ src/main/scala/NegativeOdd.scala
@@ -0,0 +1,10 @@
+object NegativeOdd {
+  def main(args:Array[String]):Unit={
+    val num = -7 ;
+    if(num <= 0 && num %2 !=0)
+      print("the given number is both negative and odd")
+    else
+      print("The number is even")
+
+  }
+}
