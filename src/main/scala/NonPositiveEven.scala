9e32cb52ebe9c04409b13d8a9884179fd50ad410
saika <sxp42750@ucmo.edu>
2024-08-15 15:57:02 -0400

    Number is Non-Positive AND Even

---
+++ src/main/scala/NonPositiveEven.scala
@@ -0,0 +1,10 @@
+object NonPositiveEven {
+  def main(args:Array[String]):Unit={
+    val num = -6;
+     if(num < 0 || num % 2 ==0)
+      print("The number is non Positive or even");
+    else
+      print("The number is Positive")
+
+  }
+}
