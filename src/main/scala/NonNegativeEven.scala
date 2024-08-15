672d9a802637e6173c490aa89b7d3b4b33ddf3d3
saika <sxp42750@ucmo.edu>
2024-08-15 15:51:29 -0400

    Non-Negative OR Even Number

---
+++ src/main/scala/NonNegativeEven.scala
@@ -0,0 +1,10 @@
+object NonNegativeEven {
+  def main(args:Array[String]):Unit={
+    val num = -8
+    if(num > 0 || num % 2 ==0)
+      print("The number is non negative or even");
+    else
+      print("The number is negative")
+
+  }
+}
